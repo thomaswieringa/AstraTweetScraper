cad794a11108d40cd0b7e9cc439bece9f348f5b7
Thomas <thomas1998@me.com>
2020-08-28 16:14:01 +0200

    yes

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-import requests
-import pandas as pd
 import twint
 import time
-import json
 import datetime
 from pymongo import MongoClient
-import pymongo
-import string
-import re
-from langdetect import detect, detect_langs
-from contextlib import suppress
-
-from langdetect.lang_detect_exception import LangDetectException
-from nltk import PorterStemmer, word_tokenize
-from nltk.corpus import stopwords
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -22,8 +10,8 @@ nltk.download('punkt')
 def executequery(query,tweets):
     c = twint.Config()
     c.Username = query["user"]
-    c.Since = datetime.utcfromtimestamp(query["datefrom"]).strftime('%Y-%m-%d %H:%M:%S')
-    c.Until =  datetime.utcfromtimestamp(query["dateto"]).strftime('%Y-%m-%d %H:%M:%S')
+    c.Since = datetime.datetime.utcfromtimestamp(query["startDate"]).strftime('%Y-%m-%d %H:%M:%S')
+    c.Until = datetime.datetime.utcfromtimestamp(query["endDate"]).strftime('%Y-%m-%d %H:%M:%S')
     c.Pandas = True
 
     # Run
@@ -32,14 +20,17 @@ def executequery(query,tweets):
     # now you will have some tweets
     tweets_df = twint.storage.panda.Tweets_df
 
-    result = {}
+    result = []
     for index, row in tweets_df.iterrows():
         tweet = row['tweet']
         date = row['date']
-        result.update( {"date": date,
+        pattern = '%Y-%m-%d %H:%M:%S'
+        epoch = int(time.mktime(time.strptime(date, pattern)))
+        result.append( {"date": epoch,
                   "tweet": tweet})
 
-    tweets.insert_many(result)
+    if result:
+        tweets.insert_many(result)
 
 
 
@@ -49,13 +40,12 @@ def main():
         "mongodb+srv://thomas:thomas123@cluster0-0kckv.mongodb.net/test?retryWrites=true&w=majority")
 
     tweets = client['twitter']['scrapedTweets']
-    todoCol = client['twitter']['usersToScrapeV2']
+    todoCol = client['twitter']['usersToScrape1YearV2']
     currentQuery = todoCol.find_one_and_delete({})
     while currentQuery != None:
         t0 = time.time()
-        executequery(currentQuery)
+        executequery(currentQuery, tweets)
         t1 = time.time()
-        print("User {} took {}".format(currentQuery["user"],tweets, t1-t0))
         currentQuery = todoCol.find_one_and_delete({})
 
 

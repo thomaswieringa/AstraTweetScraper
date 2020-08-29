536a2f8e9a89a4bc7df841717dbebf8512e13cc2
Thomas <thomas1998@me.com>
2020-08-29 14:13:43 +0200

    yes

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def executequery(query,tweets):
     c.Since = datetime.datetime.utcfromtimestamp(query["startDate"]).strftime('%Y-%m-%d %H:%M:%S')
     c.Until = datetime.datetime.utcfromtimestamp(query["endDate"]).strftime('%Y-%m-%d %H:%M:%S')
     c.Pandas = True
+    c.Hide_output = True
 
     # Run
     twint.run.Search(c)
@@ -39,14 +40,16 @@ def main():
     client = MongoClient(
         "mongodb+srv://thomas:thomas123@cluster0-0kckv.mongodb.net/test?retryWrites=true&w=majority")
 
-    tweets = client['twitter']['scrapedTweetsV2']
-    todoCol = client['twitter']['usersToScrape1YearV3']
+    tweets = client['twitter']['scrapedTweetsFinal']
+    todoCol = client['twitter']['usersToScrape1YearNoDuplicates']
     currentQuery = todoCol.find_one_and_delete({})
     while currentQuery != None:
         t0 = time.time()
         executequery(currentQuery, tweets)
         t1 = time.time()
+        print("User {} took {}".format(currentQuery["user"], t1 - t0))
         currentQuery = todoCol.find_one_and_delete({})
+
 
 
 
